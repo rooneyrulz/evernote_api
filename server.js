508df66c9193ff157e61a191ce7045910c688689
ruzny <ruzny@ruzny.com>
2020-08-31 20:06:13 +0530

    mongo connection established

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { createServer } = require('http');
 const dotenv = require('dotenv');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 // GRAPHQL
 const { graphqlHTTP } = require('express-graphql');
@@ -69,11 +70,33 @@ app.use(
     })
 );
 
-server.listen(process.env.PORT || 5000, () =>
-    console.log(
-        `Server running on ${process.env.NODE_ENV} port ${process.env.PORT || 5000}`
-        .yellow
-    )
-);
+async function initMongoConnection() {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        if (conn.connection) {
+            console.log(
+                `mongo connection established on ${conn.connection.host} ${conn.connection.port}..`
+                .inverse.cyan
+            );
+            server.listen(process.env.PORT || 5000, () =>
+                console.log(
+                    `Server running on ${process.env.NODE_ENV} port ${
+            process.env.PORT || 5000
+          }`.yellow
+                )
+            );
+        }
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+initMongoConnection();
 
 module.exports = server;
