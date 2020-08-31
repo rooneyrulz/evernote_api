33c0b7137c7c8f1781d8bb76d8aef330963a9dc3
ruzny <ruzny@ruzny.com>
2020-08-31 18:36:30 +0530

    working on graphqlHTTP | building schema | rootValues

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const { createServer } = require('https');
+const { createServer } = require('http');
 const dotenv = require('dotenv');
+const logger = require('morgan');
+
+// GRAPHQL
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+require('colors');
 
 // Load .ENV
 dotenv.config({ path: './config/config.env' });
@@ -8,6 +15,30 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 const server = createServer(app);
 
-server.listen(process.env.PORT || 5000, () =>
-  console.log(`Server running on port ${process.env.PORT || 5000}`)
+if (process.env.NODE_ENV === 'development') app.use(logger('dev'));
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+          type RootQuery {}
+          
+          type RootMutation {}
+
+          schema {
+            query: RootQuery
+            mutation: RootMutation
+          }
+        `),
+        rootValue: {},
+    })
 );
+
+server.listen(process.env.PORT || 5000, () =>
+    console.log(
+        `Server running on ${process.env.NODE_ENV} port ${process.env.PORT || 5000}`
+        .yellow
+    )
+);
+
+module.exports = server;
